a852eaa4057123ca5211e24d510ad83aa3750791
Rishabh Malviya <rishabh.malviya.america@gmail.com>
2026-01-29 22:00:43 +0530

    minor refactor

--- raag_midi_gen/utils/midi_utils.py
+++ raag_midi_gen/utils/midi_utils.py
@@ -4,10 +4,12 @@ import muspy
 
 
 
-def play_midi_file(midi_file_path: str, audio_file_path: str = 'tmp.wav', sf2_path: str = '/usr/share/sounds/sf2/default-GM.sf2'):
-    """
-    Other possible SF2 File Paths are `TimGM6mb.sf2` and `FluidR3_GM.sf2`
-    """
+def play_midi_file(
+    midi_file_path: str,
+    audio_file_path: str = 'tmp.wav',
+    sf2_file: str = 'default-GM.sf2'  # Other possible SF2 File Paths are `TimGM6mb.sf2` and `FluidR3_GM.sf2`
+):
+    sf2_path = f'/usr/share/sounds/sf2/{sf2_file}'
 
     fs = FluidSynth(sf2_path)
     fs.midi_to_audio(midi_file_path, audio_file_path)
